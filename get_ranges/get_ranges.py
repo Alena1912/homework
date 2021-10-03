981d0020339148882a4dfdff56b5c4f61b566777
Alena1912 <veter.elena2013@yandex.ru>
2021-10-04 00:18:26 +0300

    get_ranges

--- get_ranges/get_ranges.py
+++ get_ranges/get_ranges.py
@@ -8,6 +8,28 @@
 - [2, 3, 8, 9]) -> "2-3, 8-9"
 """
 
-
 def get_ranges(l: list) -> str:
-    pass
+    m = []
+    for i in l:
+        if not m:
+            m.append([i])
+        elif int(i) - pred_i == 1:
+            m[-1].append(i)
+        else:
+            m.append([i])
+        pred_i = int(i)
+
+    final_ranges = ''
+
+    for range_ in m:
+        if len(range_) == 1:
+            str_range = str(range_[0])
+        elif len(range_) >= 2:
+            str_range = f"{range_[0]}-{range_[-1]}"
+
+        if final_ranges=='':
+            final_ranges=str_range
+        else:
+            final_ranges = final_ranges + ', ' + str_range
+
+    return final_ranges
