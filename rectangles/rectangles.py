261d3c42bbb5ec39493f608648803a3cb23ed479
Alena1912 <veter.elena2013@yandex.ru>
2021-10-03 18:37:42 +0300

    rectangles

--- rectangles/rectangles.py
+++ rectangles/rectangles.py
@@ -24,11 +24,44 @@ def rect_inside(a: Rect, b: Rect) -> bool:
     """
     Checks if whole rectangle `b` is within rectangle `a`.
     """
-    pass
+    x1 = a.x
+    x2 = a.x + a.w
+    y1 = a.y
+    y2 = a.y + a.h
+
+    xx1 = b.x
+    xx2 = b.x + b.w
+    yy1 = b.y
+    yy2 = b.y + b.h
+
+    if all([x1 <= xx1,
+            y1 <= yy1,
+            x2 >= xx2,
+            y2 >= yy2]) or all([xx1 <= x1,
+                                yy1 <= y1,
+                                xx2 >= x2,
+                                yy2 >= y2]):
+        return True
+    return False
 
 
 def rects_intersect(a: Rect, b: Rect) -> bool:
     """
     Checks if 2 rectangles `a` and `b` have at least a single intersection point.
     """
-    pass
+    x1 = a.x
+    x2 = a.x + a.w
+    y1 = a.y
+    y2 = a.y + a.h
+
+    xx1 = b.x
+    xx2 = b.x + b.w
+    yy1 = b.y
+    yy2 = b.y + b.h
+
+    if any([all([x1 <= xx1 <= x2, y1 <= yy1 <= y2]),
+            all([x1 <= xx2 <= x2, y1 <= yy1 <= y2]),
+            all([xx1 <= x1 <= xx2, yy1 <= y2 <= yy2]),
+            all([xx1 <= x2 <= xx2, yy1 <= y2 <= yy2])]):
+        return True
+    return False
