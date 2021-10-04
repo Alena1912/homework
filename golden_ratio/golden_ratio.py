447ffd714d96e8a451f165b1464b5b4d2225161d
Alena1912 <veter.elena2013@yandex.ru>
2021-10-04 03:51:43 +0300

    golden_ratio

--- golden_ratio/golden_ratio.py
+++ golden_ratio/golden_ratio.py
@@ -14,18 +14,26 @@ https://en.wikipedia.org/wiki/Golden_ratio#Relationship_to_Fibonacci_sequence
 """
 
 # Golden Ratio
-GOLDEN = (1 + 5 ** 0.5) / 2
+GOLDEN = (1 + 5 ** 0.5) / 2  # (a+b)/a or b/a
+import math
 
 
 def fib_pair_gen():
     """
     Generator to produce Fibonacci numbers indefinitely.
     """
-    pass
+    a = b = 1
+
+    while 1:
+        yield a, b
+        a, b = b, a + b
 
 
 def fib_phi_calc(precision: float) -> (int, int):
     """
     Returns 2 consecutive Fibonacci numbers to calculate Golden Ratio with a given precision.
     """
-    pass
+    for a, b in fib_pair_gen():  # pary iz generatora
+        pogr = abs(GOLDEN - b / a)  # pogreshnost'
+        if precision >= pogr:
+            return a, b
